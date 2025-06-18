3be77dc7f29a2f8643ec6531cc52cfc3ac1c2a45
kishor324 <kishorbhatt157@gmail.com>
2025-06-18 23:02:17 +0545

    Introduction of java script

---
+++ video.54/new.js
@@ -0,0 +1,3 @@
+console.log("HEllo wrold")
+console.log(" code is   also running")
+console.log(" code is lokking like a wow....")
---
+++ video.54/script.js
@@ -0,0 +1,21 @@
+alert("Hello world")
+
+console.log(" code is running")
+console.log(" code is   also running")
+console.log(" code is lokking like a wow....")
+//  console.log(" code is running")
+var a = prompt(" Enter your number")
+var isTure = confirm(" Are you sure you wnat to leave this page and blast your computer")
+if (isTure) {
+    console.log(" computer is blasting")
+
+}
+else
+ {
+    console.log(" your computer is not blasting")
+}
+console.log(" Your nunber is " + a)
+
+ document.title = " hey i am good"
+
+ document.body.style.backgroundColor="red"
