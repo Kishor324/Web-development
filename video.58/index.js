cc9fecd5514e9ec256586e926103aab6c3b3cbf9
kishor324 <kishorbhatt157@gmail.com>
2025-06-19 17:09:24 +0545

    Tutorial 58 - functions  in js

---
+++ video.58/index.js
@@ -0,0 +1,34 @@
+console.log(" This is tutorial 58 about function")
+
+function nice(ram) {
+    console.log(" Hy +  name+  + i am good "),
+        console.log(" Hy +  name+  i am  looking good "),
+        console.log("Hy + name+  i name am  also looking good  for today"),
+        console.log(" Hy + name+  harry's cousrse is curious for biggner"),
+        console.log(" Hy+  name+ i am good its okey")
+
+    
+    
+}
+function sum(a, b, c = 3)
+{
+    // console.log(a+b);
+    console.log ( a,b,c)
+    return a+b+ c 
+}
+ result1 = sum(4,2 )
+
+ result2 = sum( 4,9)
+
+ result3 = sum ( 3,4)
+    
+  console.log (" The sum of these two number is : " ,result1)
+  console.log (" The sum of these two number is : " ,result2)
+  console.log (" The sum of these two number is : " ,result3)
+
+  const func1 = (x) =>{
+    console.log(" I am an arrow function ", x)
+  }
+   func1(23);
+   func1(63);
+   func1(83);
