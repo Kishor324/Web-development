f81116b7f80a17e2c3eb92df5e5b27f78c96f4e2
kishor324 <kishorbhatt157@gmail.com>
2025-06-19 17:06:00 +0545

    Tutorial 56 - Variables , Data types in js

---
+++ video.56/index.js
@@ -0,0 +1,44 @@
+console.log(" java script variable, data types");
+
+ let a =5;
+  a= a+1
+  let b =6;
+   let c= " kishor"
+   let _a =  " shubham";
+//    var 00_a= "nothing"
+/*
+ koi pani var numeber btata suru gadu nai milo
+   */
+
+    // console.log( a+b +8)
+
+    // console.log( typeof a , typeof b , typeof c)
+    // {
+    //     var x = 66;
+    //     console.log(x)
+    // }
+    // console.log( a)
+
+    //  const a1 =6;
+    //   a1 = a1 +6:
+
+    //  not allowed to do this type statement because a1 IS Constant that's why
+
+     let x = " kishor";
+     let y = 22;
+     let z = 3.55;
+     const p = true;
+      let q = undefined;
+      let r = null;
+       console.log( x,y,z,p,q,r)
+       console.log( typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+        let o = {
+            " name": " kishor",
+            "job code ": 5600,
+            "Is_handsome": true
+        }
+        console.log(o)
+        o.salary = "100crores"
+        console.log(o)
+        o.salary = "150crores"
+        console.log(o)
