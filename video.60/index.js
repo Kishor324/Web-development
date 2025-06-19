83da051b43f5442aa2429c59a6b4d76598e67d7a
kishor324 <kishorbhatt157@gmail.com>
2025-06-19 21:39:47 +0545

    Tutorial 60- String in js

---
+++ video.60/index.js
@@ -0,0 +1,29 @@
+console.log(" this is string Tutorial")
+ let  a= "kishor";
+ console.log(a[0])
+ console.log(a[1])
+ console.log(a[2])
+ console.log(a[3])
+ console.log(a[4])
+
+  console.log (a.length)
+
+  let real_name  =" kishor"
+  let friend = "harry"
+  console.log(" his name isb" +  real_name +"  and his friends name is "+ friend + " ")
+   console.log (`his name is ${real_name} and his friend name is ${ friend}`)
+
+
+    let b = " shivam"
+
+     console.log (b.toUpperCase())
+    console.log( b.toLowerCase())
+     console.log( b.length)
+     console.log ( b.slice(4,5))
+ console.log(b.slice(1))
+
+ console.log( b.replace( "sh" , "77"))
+ console.log( b.concat( a, " muskan"," rahul"," virat"))
+
+
+  console.log(b)
