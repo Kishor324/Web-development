0fc12a4329616ac716cfdd150c946b180b092909
kishor324 <kishorbhatt157@gmail.com>
2025-06-19 17:06:58 +0545

    Tutorial 56 - loops  in js

---
+++ video.57/index.js
@@ -0,0 +1,37 @@
+console.log("I am a Tutorial in loop")
+
+// let a = 1;
+//  console.log( a+3)
+//  console.log( a+5)
+//  console.log( a+8)
+
+
+
+// for(i=0; i<100; i++){
+//      console.log(a+i)
+// }
+// let obj = {
+//     name: " kishor",
+//     role : "programmer",
+//      company: " kishor ai operter"
+
+// }
+//  for (const key in obj ){
+//     console.log(key)
+//  }
+
+//   for(const c of "kishor"){
+//     console.log(c)
+//   }
+
+//  let i=0;
+//  while(i<6) {
+//     console.log(i)
+//      i++;
+//  }
+
+ let i=10;
+ do{
+    console.log(i)
+    i++
+ }  while (i<40)
