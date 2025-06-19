3c64907dcd1238f548b37c44557ff08e7d24352e
kishor324 <kishorbhatt157@gmail.com>
2025-06-19 17:04:37 +0545

    Tutorial 55 - condtionals in js

---
+++ video.55/index.js
@@ -0,0 +1,48 @@
+console.log(" Hello i am condition tutorial");
+
+let age = 1;
+//  let grace =2;
+
+//  age +=grace
+//  console.log(age)
+//  console.log(age - grace)
+//  console.log(age * grace)
+//  console.log(age / grace)
+//  console.log(age ** grace)
+//  console.log( age % grace);
+
+/*
+ i am 
+ mutlline 
+ code
+
+*/
+if (age == 18) {
+    console.log("you can drive")
+
+}
+
+else if (age == 0) {
+    console.log(" Are you kidding me")
+}
+else if (age == 1) {
+    console.log(" Are you again kidding me")
+}
+else {
+    console.log(" You cannot drive");
+}
+
+ a=6;
+ b=8;
+  
+ let c = a > b ?( a - b) : (b  - a);
+
+ /* 
+ translate to :
+ if (a>b){
+ let c= a-b;
+ }
+  else {
+  let c = a-b;
+}
+*/
