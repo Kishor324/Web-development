1f270a3077660f86c673b125b251fcce34a21eb5
kishor324 <kishorbhatt157@gmail.com>
2025-06-21 16:12:57 +0545

    Tutorial 67 - DOM Children,parent in js

---
+++ video.67/script.js
@@ -0,0 +1,7 @@
+
+ console.log("Hello world")
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+NodeList(13) 
+document.body.firstElementChild.children
+// HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
