5ce8c530d99fc75b9ef6942793a2db503ac2e2de
kishor324 <kishorbhatt157@gmail.com>
2025-06-21 16:11:12 +0545

    Tutorial 66 - DOM in js

---
+++ video.66/index.js
@@ -0,0 +1,6 @@
+console.log( " this is DOM in js")
+document.title =" kishor is good"
+ console.log( document.title)
+ console.log( document.body)
+//  document.body.style.backgroundcolor = :"blue";
+ document.body.style.backgroundColor="green"
