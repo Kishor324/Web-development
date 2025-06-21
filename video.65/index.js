fb95d68f0e145a102b017aacf727d66886b2b0ba
kishor324 <kishorbhatt157@gmail.com>
2025-06-21 16:08:22 +0545

    Tutorial 65 -Exercise 11 factorial

---
+++ video.65/index.js
@@ -0,0 +1,7 @@
+console.log(" This is exercise 11 ")
+
+ let a = [1,2,3,4,5];
+ const  factorial =( a,b)=>{
+     return a*b;
+    }
+    console.log( a.reduce( factorial));
---
+++ video.65/index2.js
@@ -0,0 +1,13 @@
+//  second method to solve or find to factorial
+ console.log(" This is about find factorial by the help of loops");
+
+ let n = 6
+//   chnage this into any number
+ let fact = 1;
+
+ for (let i= 1; i<=n; i++) {
+    // const element = n[index];
+     fact *=i;
+    
+ }
+ console.log(` factriol of ${n} is : ${fact}`);
