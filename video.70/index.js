65907cab985fe8446915a65ee5303b6f6d472875
kishor324 <kishorbhatt157@gmail.com>
2025-06-22 17:50:07 +0545

    Tutorial 70- Excrise 12 color the box

---
+++ video.70/index.js
@@ -0,0 +1,27 @@
+console.log("This is excrise 11 ")
+//  give random color in each box
+// let box = document.getElementsByClassName("box")
+// // console.log(boxes)
+// box.style.backgroundColor = "red"
+
+document.getElementById("redbox").style.backgroundColor = "red"
+document.getElementById("greenbox").style.backgroundColor = "green"
+document.getElementById("bluebox").style.backgroundColor = "blue"
+document.getElementById("yellowbox").style.backgroundColor = "yellow"
+document.getElementById("pinkbox").style.backgroundColor = "pink"
+document.getElementById("purplebox").style.backgroundColor = "purple"
+document.getElementById("xyz").style.backgroundColor = "aqua"
+
+
+// // document.querySelector(".box").style.backgroundColor = "green";
+// // console.log(document.querySelectorAll(".box"))
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// let boxes = document.getElementsByClassName("box")
+// // console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// }) 
