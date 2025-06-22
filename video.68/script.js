523592ba962f7ba767966c3f0027a03360fffce6
kishor324 <kishorbhatt157@gmail.com>
2025-06-22 17:46:33 +0545

    Tutorial 68 - Selecting by ids,classes,and more in js

---
+++ video.68/script.js
@@ -0,0 +1,32 @@
+console.log("kishor")
+
+// let boxes =document.getElementsByClassName("box")
+
+// console.log(boxes);
+
+// boxes[2].style.background ="red"
+
+// document.getElementsById("redbox").style.backgroundcolor ="red"
+
+// document.querySelector(".box").style.background = "green";
+
+ console.log(document.querySelectorAll(".box"))
+//  document.querySelectorAll(".box").forEach(e =>{
+ document.querySelectorAll(".box").forEach(e =>{
+     e.style.backgroundColor = "green";
+ })
+//  console.log("Harry")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green";
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// }) 
